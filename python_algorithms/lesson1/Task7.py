575ab6268170f17be24ec793984809b21e2b30cc
Vladislav <9998546789@mail.ru>
2020-06-25 20:49:53 +0300

    7

--- python_algorithms/lesson1/Task7.py
+++ python_algorithms/lesson1/Task7.py
@@ -2,3 +2,16 @@
 # который ввел пользователь, високосным или не високосным.
 
 
+year = int(input("Введите год: "))
+year_type = ""
+if year % 4 != 0:
+    year_type = "usual year"
+elif year % 100 == 0:
+    if year % 400 == 0:
+        year_type = "intercalary year"
+    else:
+        year_type = "usual year"
+else:
+    year_type = "intercalary year"
+
+print(year_type)
